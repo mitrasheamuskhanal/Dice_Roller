985355b3f0da24cc2e5f84fdc8b3dfa07dfe0b61
mitrasheamuskhanalgit config --global user.name mitrasheamuskhanalgit config --global user.name mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2023-09-10 18:24:03 +0545

    added image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Random
@@ -21,6 +22,16 @@ class MainActivity : AppCompatActivity() {
     private fun roll_dice() {
        val result : TextView = findViewById(R.id.result)
         val randonInt = Random().nextInt(6 ) +1
+       val image =  when (randonInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceimage : ImageView = findViewById(R.id.image)
+        diceimage.setImageResource(image)
         result.text = randonInt.toString()
     }
 }
